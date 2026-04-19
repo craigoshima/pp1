5032b8e1609bbb46197baf5475271080adea741b
Craig Oshima <craigoshima@gmail.com>
2026-04-18 23:30:46 -0700

    Add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://craigoshima.github.io",
 	base: "/pp1",
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
