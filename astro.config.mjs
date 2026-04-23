3dfadb5ba92f2ce08c88d789d6bcb373fb2cc05b
Craig Oshima <craigoshima@gmail.com>
2026-04-23 15:32:55 -0700

    short url redirects to letter and flyer

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,19 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
+
+    redirects: {
+        "/letter-en":
+            "https://professionalsplatform.org/media/English%20Steering%20Committee-FNLetter.pdf",
+        "/letter-es":
+            "https://professionalsplatform.org/media/Spanish%20Steering%20Committee-FNLetter.pdf",
+        "/letter-fr":
+            "https://professionalsplatform.org/media/French%20Steering%20Committee-FNLetter.pdf",
+        "/flyer-en":
+            "https://professionalsplatform.org/media/2026-27%20PP%20Launch%20English-FN.pdf",
+        "/flyer-es":
+            "https://professionalsplatform.org/media/2026-27%20PP%20Launch%20Spanish-FN.pdf",
+        "/flyer-fr":
+            "https://professionalsplatform.org/media/2026-27%20PP%20Launch%20French-FN.pdf",
+    },
 })
