79f9ac4bdeb9e830e91b45fe13d1c3285dadbca2
Craig Oshima <craigoshima@gmail.com>
2026-06-10 10:44:37 -0700

    Update for July: new nav, session details, bios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config"
+import { defineConfig, passthroughImageService } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
@@ -13,6 +13,10 @@ export default defineConfig({
     i18n: {
         locales: ["en", "es", "fr"],
         defaultLocale: "en",
+    },
+
+    image: {
+        service: passthroughImageService(),
     },
 
     redirects: {
--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -9,23 +9,33 @@
 export function localizePath(pathname: string, locale: string) {
     const locales = ["es", "fr"]
     const parts = pathname.split("/").filter(Boolean)
-    console.log("LOCAALIZE")
-    console.log(pathname)
-    console.log(parts)
+    // console.log("LOCAALIZE")
+    // console.log(pathname)
+    // console.log(parts)
 
+    // If the first part is one of the locales, change it to the new locale
     if (parts.length && locales.includes(parts[0])) {
         parts[0] = locale
     } else {
         parts.unshift(locale)
     }
-    let localizedPath = `/${parts.join("/")}/`
-    console.log(localizedPath)
 
-    // strip defaultLocale
+    let localizedPath = `/${parts.join("/")}/`
+    // If new location is "en", strip it from the path
     if (locale === "en") {
         // localizedPath = localizedPath.replace(`/${defaultLocale}`, "")
         localizedPath = localizedPath.replace("/en", "")
     }
 
     return localizedPath
+}
+
+/**
+ * Strips a locale from the given pathname. Useful to facilitate comparisons
+ * @param pathname the path to de-localize
+ */
+export function stripLocaleFromPath(pathname: string) {
+    const locales = ["en", "es", "fr"]
+    const parts = pathname.split("/").filter(Boolean)
+console.log("path parts: ", parts)
 }
