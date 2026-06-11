7aa00af88079e7c39212fd51719167b549261d1e
Craig Oshima <craigoshima@gmail.com>
2026-06-11 04:16:01 -0700

    Links to announcement letters

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,12 +20,16 @@ export default defineConfig({
     },
 
     redirects: {
+        "/announcement":
+            "https://professionalsplatform.org/media/2026-07-23/English-PP-23-July-2026.pdf",
         "/en/announcement":
             "https://professionalsplatform.org/media/2026-07-23/English-PP-23-July-2026.pdf",
         "/es/announcement":
             "https://professionalsplatform.org/media/2026-07-23/Espanol-PP-23-julio-2026.pdf",
         "/fr/announcement":
             "https://professionalsplatform.org/media/2026-07-23/Francais-PP-23-juillet-2026.pdf",
+        "/faq":
+            "https://professionalsplatform.org/media/faq/Professionals_Platform_FAQ-en.docx",
         "/en/faq":
             "https://professionalsplatform.org/media/faq/Professionals_Platform_FAQ-en.docx",
         "/es/faq":
@@ -33,20 +37,12 @@ export default defineConfig({
         "/fr/faq":
             "https://professionalsplatform.org/media/faq/Professionals_Platform_FAQ-fr.docx",
         "/letter":
-            "https://professionalsplatform.org/media/English%20Steering%20Committee-FNLetter.pdf",
+            "https://professionalsplatform.org/media/2026-07-23/English23JulyLTR.pdf",
         "/en/letter":
-            "https://professionalsplatform.org/media/English%20Steering%20Committee-FNLetter.pdf",
+            "https://professionalsplatform.org/media/2026-07-23/English23JulyLTR.pdf",
         "/es/letter":
-            "https://professionalsplatform.org/media/Spanish%20Steering%20Committee-FNLetter.pdf",
+            "https://professionalsplatform.org/media/2026-07-23/Espanol23dejulioLTR.pdf",
         "/fr/letter":
-            "https://professionalsplatform.org/media/French%20Steering%20Committee-FNLetter.pdf",
-        "/flyer":
-            "https://professionalsplatform.org/media/2026-27%20PP%20Launch%20English-FN.pdf",
-        "/en/flyer":
-            "https://professionalsplatform.org/media/2026-27%20PP%20Launch%20English-FN.pdf",
-        "/es/flyer":
-            "https://professionalsplatform.org/media/2026-27%20PP%20Launch%20Spanish-FN.pdf",
-        "/fr/flyer":
-            "https://professionalsplatform.org/media/2026-27%20PP%20Launch%20French-FN.pdf",
+            "https://professionalsplatform.org/media/2026-07-23/Francais23juilletLTR.pdf"
     },
 })
